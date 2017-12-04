560b1a542afb9c3c30cde1bd76d740d26bffd0eb
David Wilkie <dwilkie@gmail.com>
2017-12-04 14:48:40 +0700

    Add test for successful OTP

--- spec/phillip_bank/cbs_conductor/request/otp_spec.rb
+++ spec/phillip_bank/cbs_conductor/request/otp_spec.rb
@@ -29,8 +29,17 @@ RSpec.describe ***REMOVED***::***REMOVED***::Request::OTP do
       expect(response).to be_a(***REMOVED***::***REMOVED***::Response::OTP)
     end
 
-    context "successful request" do
+    context "successful request", :focus, :cassette => :send_otp do
       let(:asserted_successful) { true }
+      let(:account_number) { "000010100280303" }
+      let(:phone_number) { "855715100860" }
+
+      def assert_execute!
+        super
+        expect(response).to be_successful
+      end
+
+      it { assert_execute! }
     end
 
     context "error 110", :cassette => :send_otp_110 do
